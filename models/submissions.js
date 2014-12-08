53b53206c3676d8bc53f724a7bb8d521e29bc47c
Samantha McCormick <samanthagmccormick@gmail.com>
2014-12-08 14:21:30 -0700

    Styling.. still need to make voting view function

--- models/submissions.js
+++ models/submissions.js
@@ -7,19 +7,40 @@ var submissions = [
 		votes: 0
 	},
 	{
-		name: 'Samantha',
-		url: 'http://player.vimeo.com/video/1193177?title=0&amp;byline=0&amp;portrait=0',
+		name: 'Megan',
+		url: 'http://player.vimeo.com/video/1193177?portrait=0',
 		title: 'Cranford Rose Garden',
 		description: 'This time-lapse shows three days in the life of the Cranford Rose Garden at Brooklyn Botanic Garden as thousands of roses bloom in early June. To create the unique perspective, each still frame of the video was treated with a tilt-shift lens effect in Photoshop.',
 		votes: 0
 	},
 	{
-		name: 'Samantha',
-		url: 'http://player.vimeo.com/video/1193177?title=0&amp;byline=0&amp;portrait=0',
-		title: 'A Third Video',
-		description: 'This time-lapse shows three days in the life of the Cranford Rose Garden at Brooklyn Botanic Garden as thousands of roses bloom in early June. To create the unique perspective, each still frame of the video was treated with a tilt-shift lens effect in Photoshop.',
+		name: 'Joe',
+		url: 'http://player.vimeo.com/video/106998948?portrait=0',
+		title: 'A Boulder Summer',
+		description: 'This is a tribute to the moments of summer and a glimpse into the community of Boulder, Colorado. Created for the 2014 TEDx Boulder conference by Balcony Nine Media.',
 		votes: 0
 	},
+	{
+		name: 'Amy',
+		url: 'http://player.vimeo.com/video/31066979?portrait=0',
+		title: 'Texas Cemeteries',
+		description: 'This time-lapse was shot at several cemeteries through out Texas.',
+		votes: 0
+	}, 
+	{
+		name: 'Jim',
+		url: 'http://player.vimeo.com/video/14476029?portrait=0',
+		title: 'Queensland Astrofest',
+		description: 'Astronomical time lapse of night sky over the 2010 Queensland Astrofest (A star party at Camp Duckadang in Linville, Queensland, Australia) on 6-7 August 2010. The video starts at nightfall with the galactic core of the Milky Way directly overhead moving to the West with Earth’s rotation.',
+		votes: 0
+	}, 
+	{
+		name: 'Dylan',
+		url: 'http://player.vimeo.com/video/6347263?portrait=0',
+		title: 'Sieko and Amanda\'s Wedding',
+		description: 'Shot at Noordhoek Beach in Cape Town, South Africa',
+		votes: 0
+	}
 ];
 
 module.exports = submissions;
